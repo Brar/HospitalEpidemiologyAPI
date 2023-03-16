a5475e2334f74bde3fe28e6eb2bd21c94fc154ed
Shay Rojansky <roji@roji.org>
2023-03-16 06:30:52 +0100

    Query games (#1)

--- src/HospitalEpidemiology/Program.cs
+++ src/HospitalEpidemiology/Program.cs
@@ -6,10 +6,50 @@ using NpgsqlTypes;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContextPool<HospitalEpidemiologyDb>(
     opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("HospitalEpidemiology"))
+        .LogTo(Console.WriteLine, LogLevel.Information)
+        .EnableSensitiveDataLogging()
         .UseSnakeCaseNamingConvention()
         .EnableThreadSafetyChecks(false));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
+
+var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetService<HospitalEpidemiologyDb>();
+
+var indexPatientId = 8;
+var minimumCumulativeContactDuration = TimeSpan.FromMinutes(90);
+var riskRange = new NpgsqlRange<DateTime>(
+    new(2020, 1, 1, 12, 0, 0, DateTimeKind.Utc),
+    new(2020, 1, 3, 12, 0, 0, DateTimeKind.Utc));
+
+var query =
+    from overlappingPatients in
+        // Start with the index patient's stays, filtering out any which are completely outside the risk range
+        from indexPatientStay in context.PatientRoomStays
+        where indexPatientStay.PatientId == indexPatientId
+              && indexPatientStay.Stay.Overlaps(riskRange)
+        // Join with other patient stays that have the same room and overlapping stays
+        from patientStay in context.PatientRoomStays
+        where patientStay.PatientId != indexPatientId
+              && patientStay.RoomId == indexPatientStay.RoomId
+              && patientStay.Stay.Overlaps(indexPatientStay.Stay)
+        // Group by the patient, intersecting all their stays with the risk range, projecting the intersected stays
+        // to the TimeSpan duration, and aggregating that duration
+        group patientStay.Stay by patientStay.PatientId
+        into patientStayGroup
+        select new
+        {
+            PatientId = patientStayGroup.Key,
+            AggregateOverlapTime = EF.Functions.Sum(patientStayGroup.Select(s => s.Intersect(riskRange))
+                .Select(s => s.UpperBound - s.LowerBound))
+        }
+    // From the above subquery, filter out patients which which didn't overlap for enough tmie
+    where overlappingPatients.AggregateOverlapTime > minimumCumulativeContactDuration
+    select overlappingPatients;
+
+_ = query.ToArray();
+
+Environment.Exit(0);
 
 app.MapGet("/roomContacts", async ([FromQuery] int indexPatientId, [FromQuery] DateTime? riskFromInclusive, [FromQuery] DateTime? riskToExclusive, [FromQuery] TimeSpan minimumCumulativeContactDuration, HospitalEpidemiologyDb db) =>
 {
